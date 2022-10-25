7abe50995ddc7d6ed51de4d69bd46de00814b9f5
JM <jmsofts20@gmail.com>
2022-10-25 12:17:39 -0500

    Renamed Project...

---
+++ src/components/ProjectEcomm.js
@@ -0,0 +1,82 @@
+import React from "react";
+import { Navbar, NavbarBrand, Col, Row } from "reactstrap";
+
+const ProjectA = () => {
+
+    return(
+        <React.Fragment>
+            <div className="imgSpan bg-warning">
+                <p>image for project A</p>
+            </div>
+            <Navbar sticky='top'>
+                <NavbarBrand>
+                    <h2> E-Commerce Site 
+                        <span className="p0m0 sig"> ...Web </span>
+                    </h2>
+                </NavbarBrand>
+            </Navbar>
+            <Row>
+                <Col>
+                    <p>This is the landing page</p>
+                    <p>This is the landing page</p>
+                    <p>This is the landing page</p>
+                </Col>
+                <Col>
+                    <p>This is the landing page</p>
+                    <p>This is the landing page</p>
+                    <p>This is the landing page</p>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <p>This is the landing page</p>
+                    <p>This is the landing page</p>
+                    <p>This is the landing page</p>
+                </Col>
+                <Col>
+                    <p>This is the landing page</p>
+                    <p>This is the landing page</p>
+                    <p>This is the landing page</p>
+                </Col>
+                <Col>
+                    <p>This is the landing page</p>
+                    <p>This is the landing page</p>
+                    <p>This is the landing page</p>
+                </Col>
+            </Row>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+        </React.Fragment>
+    );
+}
+
+export default ProjectA;
---
+++ src/components/ProjectMPesaTRS.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Navbar, NavbarBrand, Col, Row } from "reactstrap";
+
+const ProjectC = () => {
+
+    return(
+        <React.Fragment>
+            <div className="imgSpan">
+                <p>image for M-PESA Transactions Recording System</p>
+            </div>
+            <Navbar sticky='top'>
+                <NavbarBrand>
+                    <h2> M-PESA Transactions Recording System
+                        <span className="p0m0 sig"> ...Android & iOS </span>
+                    </h2>
+                </NavbarBrand>
+            </Navbar>
+            <Row>
+                <Col>
+                    <p>This is Offline-First</p>
+                    <p>This is the landing page</p>
+                    <p>This is the landing page</p>
+                </Col>
+                <Col>
+                    <p>This is the landing page</p>
+                    <p>This is the landing page</p>
+                    <p>This is the landing page</p>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <p>This is the landing page</p>
+                    <p>This is the landing page</p>
+                    <p>This is the landing page</p>
+                </Col>
+                <Col>
+                    <p>This is the landing page</p>
+                    <p>This is the landing page</p>
+                    <p>This is the landing page</p>
+                </Col>
+                <Col>
+                    <p>This is the landing page</p>
+                    <p>This is the landing page</p>
+                    <p>This is the landing page</p>
+                </Col>
+            </Row>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+        </React.Fragment>
+    );
+}
+
+export default ProjectC;
