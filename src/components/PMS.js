3e046391b19ff6f53d28b0836af876faef6a64d8
JM <jmsofts20@gmail.com>
2022-10-25 11:59:06 -0500

    Project page draft

---
+++ src/components/PMS.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { Navbar, NavbarBrand, Col, Row } from "reactstrap";
+
+const ProjectB = () => {
+
+    return(
+        <React.Fragment>
+            <div className="imgSpan">
+                <p>image for Personal Management System</p>
+            </div>
+            <Navbar sticky='top'>
+                <NavbarBrand>
+                    <h2> Personal Management System
+                        <span className="p0m0 sig"> ...Android & iOS </span>
+                    </h2>
+                </NavbarBrand>
+            </Navbar>
+            <Row>
+                <Col>
+                    <p>This is the landing page</p>
+                    <p>This is the landing page</p>
+                    <p>This is the landing page</p>
+                </Col>
+                <Col>
+                    <p>This is the landing page</p>
+                    <p>This is the landing page</p>
+                    <p>This is the landing page</p>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <p>This is the landing page</p>
+                    <p>This is the landing page</p>
+                    <p>This is the landing page</p>
+                </Col>
+                <Col>
+                    <p>This is the landing page</p>
+                    <p>This is the landing page</p>
+                    <p>This is the landing page</p>
+                </Col>
+                <Col>
+                    <p>This is the landing page</p>
+                    <p>This is the landing page</p>
+                    <p>This is the landing page</p>
+                </Col>
+            </Row>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+            <p>This is the landing page</p>
+        </React.Fragment>
+    );
+}
+
+export default ProjectB;
