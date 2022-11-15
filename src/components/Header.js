da66e45a288f058ed176fe19dd2f0399c03aa76d
JM <jmsofts20@gmail.com>
2022-11-15 10:26:31 -0600

    Added nav to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,48 +1,155 @@
 import React, { useState, useRef } from "react";
-import { Nav, Navbar, NavbarBrand, NavItem, NavLink, NavbarToggler, Collapse, Container, Row, Col } from 'reactstrap';
+import { Nav, Navbar, NavbarBrand, NavItem, NavLink, NavbarToggler, Collapse, Row, Col, Card, CardHeader, CardBody, CardText, CardImg,  Media } from 'reactstrap';
 import '../scroller.css';
+import AnimatedBrand from "./AnimatedBrand";
 
 
 const Header = (props) => {
     const [isOpen, setIsOpen] = useState(false);
-    const { onScrollToRef, sectionA, sectionB, sectionC, sectionD, sectionE, sectionF } = props;
-    console.log(`this is header`)
+    const { onScrollToRef, sectionA, sectionB, sectionC, sectionD, sectionE, sectionF, sectionG, sectionH } = props;
+
+    // function lockScroll() {
+    //     document.body.classList.toggle('lock-scroll');
+    // }
 
     const handleClick = (ref) => {
         onScrollToRef(ref);
         setIsOpen(!isOpen);
+        // lockScroll();
     }
 
     return(
         <React.Fragment>
-            <Navbar sticky='top' expand='none' light className="bg-secondary">
-                <NavbarBrand className="col-8 col-md-6 bg-info">
-                    <h1>John Maina</h1>
+            <Navbar fixed='top' expand='none' light className="navbar" style={{background: 'transparent'}}>
+                <NavbarBrand className="col-10 col-lg-9 p0m0">
+                    <div >
+                        <h1 className="col col-md-6"></h1>
+                        {/* <span className="col-6 d-none d-md-inline-block"><AnimatedBrand /> </span> */}
+                    </div>
                 </NavbarBrand>
-                <NavbarToggler onClick={() => { setIsOpen(!isOpen) }} />
+                <NavbarToggler 
+                    className='bg-warning m-0'
+                    onClick={() => { 
+                        setIsOpen(!isOpen)
+                        // lockScroll()
+                    }} 
+                />
                 <Collapse 
-                    isOpen={isOpen} navbar className="col-4 col-md-6 mt-3 bg-danger" 
-                    style={{flexDirection: 'row', justifyContent: 'right'}}
+                    isOpen={isOpen} navbar className="col-2 col-lg-3 bg-success p0m0"  
+                    style={{flexDirection: 'row', justifyContent: 'right', height: '90vh'}}
                 >
-                    <Nav className="navi p0m0 " navbar >
-                        <NavItem className="nav-item bg-info">
-                            <NavLink className="nav-link" to="/projectA" onClick={() => handleClick(sectionA)}>Project A</NavLink>
-                        </NavItem>
-                        <NavItem className="nav-item">
-                            <NavLink className="nav-link" to="/projectB" onClick={() => handleClick(sectionB)}>Project B</NavLink>
-                        </NavItem>
-                        <NavItem className="nav-item">
-                            <NavLink className="nav-link" to="/projectC" onClick={() => handleClick(sectionC)}>Project C</NavLink>
-                        </NavItem>
-                        <NavItem className="nav-item">
-                            <NavLink className="nav-link" to="/projectD" onClick={() => handleClick(sectionD)}>Project D</NavLink>
-                        </NavItem>
-                        <NavItem className="nav-item">
-                            <NavLink className="nav-link" to="/projectE" onClick={() => handleClick(sectionE)}>Project E</NavLink>
-                        </NavItem>
-                        <NavItem className="nav-item">
-                            <NavLink className="nav-link" to="/projectF" onClick={() => handleClick(sectionF)}>Project F</NavLink>
-                        </NavItem>
+                    <Nav className="navi" navbar >
+                        <div className="row navRow">
+                            <div className='navCol col-12 col-sm-6 col-md-4 col-lg-3'>
+                                <Card 
+                                    className='navCard'
+                                    onClick={() => handleClick(sectionA)}
+                                >
+                                    <CardHeader className="navCardHdr">
+                                        <h3>ProjectA</h3>
+                                    </CardHeader>
+                                    <CardBody>
+                                        <CardText> MyProjectA</CardText>
+                                    </CardBody>
+                                </Card>
+                            </div>
+                            <div className='navCol col-12 col-sm-6 col-md-4 col-lg-3'>
+                                <Card 
+                                    className='navCard'
+                                    onClick={() => handleClick(sectionB)}
+                                >
+                                    <CardHeader className="navCardHdr">
+                                        <h3>ProjectB</h3>
+                                    </CardHeader>
+                                    <CardBody>
+                                        <CardText> MyProjectB</CardText>
+                                    </CardBody>
+                                </Card>
+                            </div>
+                            <div className='navCol col-12 col-sm-6 col-md-4 col-lg-3'>
+                                <Card 
+                                    className='navCard'
+                                    onClick={() => handleClick(sectionC)}
+                                >
+                                    <CardHeader className="navCardHdr">
+                                        <h3>ProjectC</h3>
+                                    </CardHeader>
+                                    <CardBody>
+                                        <CardText> MyProjectC</CardText>
+                                    </CardBody>
+                                </Card>
+                            </div>
+                            <div className='navCol col-12 col-sm-6 col-md-4 col-lg-3'>
+                                <Card 
+                                    className='navCard'
+                                    onClick={() => handleClick(sectionD)}
+                                >
+                                    <CardHeader className="navCardHdr">
+                                        <h3>ProjectD</h3>
+                                    </CardHeader>
+                                    <CardBody>
+                                        <CardText> MyProjectD</CardText>
+                                    </CardBody>
+                                </Card>
+                            </div>
+                            <div className='navCol col-12 col-sm-6 col-md-4 col-lg-3'>
+                                <Card 
+                                    className='navCard'
+                                    onClick={() => handleClick(sectionE)}
+                                >
+                                    <CardHeader className="navCardHdr">
+                                        <h3>ProjectE</h3>
+                                    </CardHeader>
+                                    <CardBody>
+                                        <CardText> MyProjectE</CardText>
+                                    </CardBody>
+                                </Card>
+                            </div>
+                            <div className='navCol col-12 col-sm-6 col-md-4 col-lg-3'>
+                                <Card 
+                                    className='navCard'
+                                    onClick={() => handleClick(sectionF)}
+                                >
+                                    <CardHeader className="navCardHdr">
+                                        <h3>ProjectF</h3>
+                                    </CardHeader>
+                                    <CardBody>
+                                        <CardText> MyProjectF</CardText>
+                                    </CardBody>
+                                </Card>
+                            </div>
+                            <div className='navCol col-12 col-sm-6 col-md-4 col-lg-3'>
+                                <Card 
+                                    className='navCard'
+                                    onClick={() => handleClick(sectionG)}
+                                >
+                                    <CardHeader className="navCardHdr">
+                                        <h3>ProjectG</h3>
+                                    </CardHeader>
+                                    <CardBody>
+                                        <CardText> MyProjectG</CardText>
+                                    </CardBody>
+                                </Card>
+                            </div>
+                            <div className='navCol col-12 col-sm-6 col-md-4 col-lg-3'>
+                                <Card 
+                                    className='navCard'
+                                    onClick={() => handleClick(sectionH)}
+                                >
+                                    <CardHeader className="navCardHdr">
+                                        <h3>ProjectH</h3>
+                                    </CardHeader>
+                                    <CardBody>
+                                        <CardText> MyProjectH</CardText>
+                                    </CardBody>
+                                </Card>
+                            </div>
+                            {/* <div className='col-12 col-sm-6 col-md-4 col-lg-3 bg-info'>
+                                <NavItem className="nav-item">
+                                    <NavLink className="nav-link" to="/projectB" onClick={() => handleClick(sectionB)}>Project B</NavLink>
+                                </NavItem>
+                            </div> */}
+                        </div>
                     </Nav>
                 </Collapse>
             </Navbar>
