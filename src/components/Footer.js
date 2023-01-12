98b3db7d870eb5892e6f59fe3328d0660bd18b5a
JM <jmsofts20@gmail.com>
2023-01-12 12:48:29 -0600

    Removed commented content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,25 +5,8 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
     return(
         <React.Fragment>
-            
-            {/* <Container fluid>
-                <Row className="bg-success">
-                    <Col xs="3">Column with .col-3 Size</Col>
-                    <Col xs="auto">.Column with col-auto Size</Col>
-                    <Col xs="3">Column with .col-3 Size</Col>
-                </Row>
-                <Row className="bg-secondary">
-                    <Col xs="6">Column with .col-6 Size</Col>
-                    <Col xs="6">Column with .col-6 Size</Col>
-                </Row>
-                <Row className="bg-success">
-                    <Col xs="6" sm="4">Column with .col-6 .col-sm-4 Size</Col>
-                    <Col xs="6" sm="4">Column with .col-6 .col-sm-4 Size</Col>
-                    <Col sm="4"> Column with .col-sm-4 Size</Col>
-                </Row>
-            </Container>
 
-            <Container fluid>
+            {/* <Container fluid>
                 <Row className="bg-danger">
                     <Col className='bg-primary'>
                         This is Container 1 in Footer
@@ -39,7 +22,7 @@ const Footer = () => {
             
             <div className='row py-4 bg-dark text-white' style={{justifyContent: 'center'}}>
                 <div className='col col-8'>
-                    <p style={{textAlign: 'center'}}>@ 2022 JOHN MAINA </p>
+                    <p style={{textAlign: 'center'}}>@ 2023 JOHN M. </p>
                 </div>
             </div>
         </React.Fragment>
