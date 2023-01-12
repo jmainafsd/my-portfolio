54c75997bbd07ace7904e69ea3045a39c9ce48f2
JM <jmsofts20@gmail.com>
2023-01-12 12:11:47 -0600

    Edited Sections and their respective ref(s)

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,8 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { ScrollToTop, ScrollToRef } from './Scrolls';
+import { Container } from 'reactstrap';
 import Header from './Header';
 import Footer from './Footer';
-import HomeCarousel from './Home';
 import ProjectEcomm from './ProjectEcomm';
 import ProjectPMS from './ProjectPMS';
 import ProjectMPesaTRS from './ProjectMPesaTRS';
@@ -11,6 +11,7 @@ import Landing from './Landing';
 import SampleProjects from './SampleProjects';
 import WhatIDo from './WhatIDo';
 import Technologies from './Technologies';
+import ContactMe from './ContactMe';
 
 const Main = () => {
     const sectionA = useRef(null);
@@ -18,21 +19,16 @@ const Main = () => {
     const sectionC = useRef(null);
     const sectionD = useRef(null);
     const sectionE = useRef(null);
-    const sectionF = useRef(null);
-    const sectionG = useRef(null);
-    const sectionH = useRef(null);
 
 
     return (
-        <React.Fragment>
+        <Container fluid>
             
             {/* <div className='circle bg-info'>
                 <h2 className="brand-position brand-style"> John Maina</h2>
                 <h2 className="sub-brand-position sub-brand-style"> Fullstack Developer</h2>
             </div> */}
-
-            <ScrollToTop />
-            <Landing />
+            {/* <HomeCarousel /> */}
 
             <Header
                 onScrollToRef = {(ref) => ScrollToRef(ref) }
@@ -41,25 +37,25 @@ const Main = () => {
                 sectionC = {sectionC}
                 sectionD = {sectionD}
                 sectionE = {sectionE}
-                sectionF = {sectionF}
-                sectionG = {sectionG}
-                sectionH = {sectionH}
             />
 
-            {/* <HomeCarousel /> */}
+            <ScrollToTop />
 
-            <section ref={sectionA}> <WhatIDo /> </section>
-            <section ref={sectionB}> <Technologies /> </section>
-            <section ref={sectionC}> <SampleProjects /> </section>
-            <section ref={sectionD}> <ProjectEcomm /> </section>
-            <section ref={sectionE}> <ProjectMPesaTRS /> </section>
-            <section ref={sectionF}> <ProjectPMS /> </section>
-            <section ref={sectionG}> <ProjectRetailPOS /> </section>
-            <section ref={sectionH}> <ProjectEcomm /> </section>
+            <div className="sxn sxnA" ref={sectionA}> <Landing /> </div>
+            <div className="sxn sxnB" ref={sectionB}> <WhatIDo /> </div>
+            <div className="sxn sxnC" ref={sectionC}> <Technologies /> </div>
+            <div className="sxn sxnD" ref={sectionD}> <SampleProjects /> </div>
+            <div className="sxn sxnE" ref={sectionE}> <ContactMe /> </div>
+            
+            {/* <div className="section" ref={sectionD}> <ProjectEcomm /> </div>
+            <div className="section" ref={sectionE}> <ProjectMPesaTRS /> </div>
+            <div className="section" ref={sectionF}> <ProjectPMS /> </div>
+            <div className="section" ref={sectionG}> <ProjectRetailPOS /> </div>
+            <div className="section" ref={sectionH}> <ProjectEcomm /> </div> */}
 
             <Footer />
 
-        </React.Fragment>
+        </Container>
     );
 }
 
