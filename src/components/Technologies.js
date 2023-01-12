5da429d1f5049a29df7fd4c35d0242a9bdc23d4d
JM <jmsofts20@gmail.com>
2023-01-12 12:14:58 -0600

    Removed modal and added new functionalities to tech comp

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Navbar, NavbarBrand, Card, CardHeader, CardBody, Table, Button, Modal, ModalHeader, ModalBody, Label, Col, Row, ModalFooter  } from "reactstrap";
+import { Navbar, NavbarBrand, Card, CardHeader, CardBody, Table, Button, Modal, ModalHeader, ModalBody, Label, Col, Row, ModalFooter, Container  } from "reactstrap";
 // import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 import TechModal from "./TechModal";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -13,14 +13,106 @@ export default function Technologies() {
 
     return(
         <React.Fragment>
-            <div className="imgSpan">
-                <Row className=""><p>image for WHAT I USE</p></Row>
+            <div className="techSpan">
+                <Row className="p-0 m-0">
+                    <Col className="left"></Col>
+                    <Col className="right"></Col>
+                </Row>
             </div>
-            <Navbar sticky='top'>
-                <NavbarBrand>
-                    <h2 className="col-12"> Some Technologies I Utilize </h2>
-                </NavbarBrand>
-            </Navbar>
+            <Row className="p-0 techRow">
+                    
+                <Navbar sticky="top" className=" techNavbar">
+                    <Container className="p-0">
+                        <div className='diamond diamondOne'><p className="p1">That I Use</p></div>
+                        <div className='diamond diamondTwo'><p className="p2">Technologies</p></div>
+                    </Container>
+                </Navbar>
+
+                <Col className="col-12">
+                    <Row className="techDiv">
+                        <Col className="col col-6 col-md-4 col-lg-3 techCol">
+                            <Card className="techItem bg-warning">
+                                <img className="techItemImg" src="/assets/images/javascript.png"/>
+                                <p>JavaScript</p>
+                            </Card>
+                        </Col>
+                        <Col className="col col-6 col-md-4 col-lg-3 techCol">
+                            <Card className="techItem bg-secondary">
+                                <img className="techItemImg" src="/assets/images/react.png"/>
+                                <p style={{color: 'skyblue'}}>React</p>
+                            </Card>
+                        </Col>
+                        <Col className="col col-6 col-md-4 col-lg-3 techCol">
+                            <Card className="techItem bg-dark">
+                                <img className="techItemImg" src="/assets/images/react.png"/>
+                                <p style={{color: 'skyblue'}}>React Native</p>
+                            </Card>
+                        </Col>
+                        <Col className="col col-6 col-md-4 col-lg-3 techCol">
+                            <Card className="techItem bg-white">
+                                <img className="techItemImg" src="/assets/images/nodejs.png"/>
+                                <p>Node JS</p>
+                            </Card>
+                        </Col>
+                        <Col className="col col-6 col-md-4 col-lg-3 techCol">
+                            <Card className="techItem bg-white">
+                                <img className="techItemImg" src="/assets/images/express.png"/>
+                                <p>Express Js</p>
+                            </Card>
+                        </Col>
+                        <Col className="col col-6 col-md-4 col-lg-3 techCol">
+                            <Card className="techItem bg-white">
+                                <img className="techItemImg" src="/assets/images/Mongodb.png"/>
+                                <p>Databases</p>
+                            </Card>
+                        </Col>
+                        <Col className="col col-6 col-md-4 col-lg-3 techCol">
+                            <Card className="techItem">
+                                <img className="techItemImg" src="/assets/images/json160.gif"/>
+                                <p>JSON</p>
+                            </Card>
+                        </Col>
+                        <Col className="col col-6 col-md-4 col-lg-3 techCol">
+                            <Card className="techItem">
+                                <img className="techItemImg" src="/assets/images/redux.png"/>
+                                <p>Redux</p>
+                            </Card>
+                        </Col>
+                        <Col className="col col-6 col-md-4 col-lg-3 techCol">
+                            <Card className="techItem bg-white">
+                                <img className="techItemImg" src="/assets/images/hooks.png"/>
+                                <p className="pt-3">React Hooks</p>
+                            </Card>
+                        </Col>
+                        <Col className="col col-6 col-md-4 col-lg-3 techCol">
+                            <Card className="techItem bg-warning">
+                                <img className="techItemImg" src="/assets/images/htmlIcon.png"/>
+                                <p>Html</p>
+                            </Card>
+                        </Col>
+                        <Col className="col col-6 col-md-4 col-lg-3 techCol">
+                            <Card className="techItem bg-warning">
+                                {/* <i class="fa fa-brands fa-css3 fa-lg" /> */}
+                                <img className="techItemImg" src="/assets/images/css3.png"/>
+                                <p>CSS</p>
+                            </Card>
+                        </Col>
+                        <Col className="col col-6 col-md-4 col-lg-3 techCol">
+                            <Card className="techItem bg-warning">
+                                <img className="techItemImg" src="/assets/images/bootstrap.png"/>
+                                <p>Bootstrap</p>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+            <Row className="pt-5 pb-5 techBtnRow">
+                <Col className="centerJustified ">
+                    <Button outline size={'lg'} id="viewMoreBtn" className="fa fa-eye fa-lg px-5" onClick={toggleModal}><span> View More </span></Button>
+                </Col>
+            </Row>
+
+
             <Modal 
                 className="m-0"
                 contentClassName="customModal"
@@ -42,86 +134,6 @@ export default function Technologies() {
                 </ModalFooter>
             </Modal>
 
-            <Row>
-                <Col className="col col-6 col-md-4 col-lg-3 techCol">
-                    <Card className="techItem bg-warning">
-                        <img className="techItemImg" src="/assets/images/javascript.png"/>
-                        <p>JavaScript</p>
-                    </Card>
-                </Col>
-                <Col className="col col-6 col-md-4 col-lg-3 techCol">
-                    <Card className="techItem bg-secondary">
-                        <img className="techItemImg" src="/assets/images/react.png"/>
-                        <p style={{color: 'skyblue'}}>React</p>
-                    </Card>
-                </Col>
-                <Col className="col col-6 col-md-4 col-lg-3 techCol">
-                    <Card className="techItem bg-dark">
-                        <img className="techItemImg" src="/assets/images/react.png"/>
-                        <p style={{color: 'skyblue'}}>React Native</p>
-                    </Card>
-                </Col>
-                <Col className="col col-6 col-md-4 col-lg-3 techCol">
-                    <Card className="techItem bg-white">
-                        <img className="techItemImg" src="/assets/images/nodejs.png"/>
-                        <p>Node JS</p>
-                    </Card>
-                </Col>
-                <Col className="col col-6 col-md-4 col-lg-3 techCol">
-                    <Card className="techItem bg-white">
-                        <img className="techItemImg" src="/assets/images/express.png"/>
-                        <p>Express Js</p>
-                    </Card>
-                </Col>
-                <Col className="col col-6 col-md-4 col-lg-3 techCol">
-                    <Card className="techItem bg-white">
-                        <img className="techItemImg" src="/assets/images/Mongodb.png"/>
-                        <p>Databases</p>
-                    </Card>
-                </Col>
-                <Col className="col col-6 col-md-4 col-lg-3 techCol">
-                    <Card className="techItem">
-                        <img className="techItemImg" src="/assets/images/json160.gif"/>
-                        <p>JSON</p>
-                    </Card>
-                </Col>
-                <Col className="col col-6 col-md-4 col-lg-3 techCol">
-                    <Card className="techItem">
-                        <img className="techItemImg" src="/assets/images/redux.png"/>
-                        <p>Redux</p>
-                    </Card>
-                </Col>
-                <Col className="col col-6 col-md-4 col-lg-3 techCol">
-                    <Card className="techItem bg-white">
-                        <img className="techItemImg" src="/assets/images/hooks.png"/>
-                        <p className="pt-3">React Hooks</p>
-                    </Card>
-                </Col>
-                <Col className="col col-6 col-md-4 col-lg-3 techCol">
-                    <Card className="techItem bg-warning">
-                        <img className="techItemImg" src="/assets/images/htmlIcon.png"/>
-                        <p>Html</p>
-                    </Card>
-                </Col>
-                <Col className="col col-6 col-md-4 col-lg-3 techCol">
-                    <Card className="techItem bg-warning">
-                        {/* <i class="fa fa-brands fa-css3 fa-lg" /> */}
-                        <img className="techItemImg" src="/assets/images/css3.png"/>
-                        <p>CSS</p>
-                    </Card>
-                </Col>
-                <Col className="col col-6 col-md-4 col-lg-3 techCol">
-                    <Card className="techItem bg-warning">
-                        <img className="techItemImg" src="/assets/images/bootstrap.png"/>
-                        <p>Bootstrap</p>
-                    </Card>
-                </Col>
-            </Row>
-            <Row className="py-4">
-                <Col className="centerJustified">
-                    <Button outline color='primary' size={'lg'} id="viewMoreBtn" className="fa fa-eye fa-lg px-5" onClick={toggleModal} style={{}}> View More</Button>
-                </Col>
-            </Row>
         </React.Fragment>
     );
 }
